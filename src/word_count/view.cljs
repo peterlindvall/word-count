1409cbf83575bc8edd938e20480033df0f6b474b
Peter Lindvall <peter.lindvall@gmail.com>
2023-03-25 17:21:31 +0100

    Added textarea

--- src/word_count/view.cljs
+++ src/word_count/view.cljs
@@ -4,4 +4,13 @@
 (defn app-component
   []
   (let [db (deref db-atom)]
-    [:div (:name db)]))
+    [:div
+     [:h1 "Word count"]
+     ;[:div (str db)]
+     [:textarea {:style     {:width  "100%"
+                             :height "400px"}
+                 :value     (:value db)
+                 :on-change (fn [e]
+                              (let [value (.-value (.-target e))]
+                                (swap! db-atom assoc :value value)))}]
+     [:div (count (:value db))]]))
